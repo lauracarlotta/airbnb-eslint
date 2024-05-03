69dcd23265451b7a6b873bd79ecc6f9c0d575bd1
Laura Carlotta <carlotta.custodio@gmail.com>
2024-05-02 21:22:23 -0300

    feat: Add challenge 4 file

---
+++ challenge_4.py
@@ -0,0 +1,6 @@
+"""
+DESAFIO 4
+Crie um programa que solicita ao usuário que insira um número inteiro positivo e,
+em seguida, calcula e exibe o fatorial desse número. (O fatorial de um número é o
+produto de todos os números inteiros positivos de 1 até o próprio número.)
+"""
