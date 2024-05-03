3362b500e77a2ccb056423286ebbcee813260788
Laura Carlotta <carlotta.custodio@gmail.com>
2024-05-02 21:18:56 -0300

    feat: Add challenge 2 file

---
+++ challenge_2.py
@@ -0,0 +1,6 @@
+"""
+DESAFIO 2
+Crie um programa que solicita ao usuário que insira um número inteiro e, em
+seguida, verifica se o número é par ou ímpar.
+O programa deve exibir uma mensagem indicando se o número é par ou ímpar.
+"""
