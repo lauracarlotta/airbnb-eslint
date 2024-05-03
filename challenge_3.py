570e6f6fccbeee15e111a6deeef7bc0b480f0407
Laura Carlotta <carlotta.custodio@gmail.com>
2024-05-02 21:20:59 -0300

    feat: Add challenge 3 file

---
+++ challenge_3.py
@@ -0,0 +1,6 @@
+"""
+DESAFIO 3
+Crie um programa que verifica se uma palavra fornecida pelo usuário é
+ um palíndromo ou não.(Um palíndromo é uma palavra que é lida da mesma forma tanto
+ da esquerda para a direita quanto da direita para a esquerda.)
+"""
